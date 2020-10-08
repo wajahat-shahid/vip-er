dc68f4601d48bdca076af94f0c477ceb42595094
Muhammad Wajahat Shahid <55183024+Wajishahid10@users.noreply.github.com>
2020-10-09 01:51:28 +0500

    Settled Ambigious Variables

--- Actvity Scheduler.py
+++ Actvity Scheduler.py
@@ -1,16 +1,15 @@
-def scheduleActivities(a,s,f):
-    n = len(f)
+def scheduleActivities(subject_array,star,final):
+    n = len(final)
 
     Array=[0]*n
     i = 0
-    Array[i]=a[i]
+    Array[i]=subject_array[i]
     
-
     for j in range(n):
         
-        if s[j] >= f[i]:
+        if star[j] >= final[i]:
             i = j
-            Array[i]=a[j]
+            Array[i]=subject_array[j]
 
     return Array
   
@@ -20,8 +19,8 @@ def print_Array(A):
          if A[i]!=0:
              print(A[i])
   
-a = ["Maths","Enslish","Biology","Chemistry","Geography","Music"]
-s = [1, 3, 0, 5, 8, 5] 
-f = [2, 4, 6, 7, 9, 9]
-Array = scheduleActivities(a,s,f)
+subject_array = ["Maths","Enslish","Biology","Chemistry","Geography","Music"]
+star = [1, 3, 0, 5, 8, 5] 
+final = [2, 4, 6, 7, 9, 9]
+Array = scheduleActivities(subject_array,star,final)
 print_Array(Array)
